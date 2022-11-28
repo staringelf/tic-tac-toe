509b80700a31d784a1350a395ae00a53ed893246
Manish Sajwan <manish.sajwan@protonmail.com>
2022-11-28 16:33:10 +0530

    allow players to take turns

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,14 +6,15 @@ class Board extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares}); 
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({squares: squares, xIsNext: !this.state.xIsNext}); 
   }
 
   renderSquare(i) {
@@ -27,7 +28,7 @@ class Board extends React.Component {
 
   render () {
 
-    const status = 'X';
+    const status = this.state.xIsNext ? 'X' : 'O';
 
     return (
       <div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Square(props) {
   return (
-    <button className="square" onClick={() => this.props.onClick()}>
-      {this.props.value}
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value} 
     </button>
   )
 }
