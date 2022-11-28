2682608c3b683dcf1ebfe65bf29ce1f02f59c068
Manish Sajwan <manish.sajwan@protonmail.com>
2022-11-28 16:12:24 +0530

    first commit

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 class Square extends React.Component {
+
   render () {
     return (
-      <button className="square">
-      
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     )
   }
