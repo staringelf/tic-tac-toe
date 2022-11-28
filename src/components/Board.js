5a785ed9f845deb71a0469159036fefde1beeb22
Manish Sajwan <manish.sajwan@protonmail.com>
2022-11-28 17:34:10 +0530

    calculate a winner

--- src/components/Board.js
+++ src/components/Board.js
@@ -27,8 +27,13 @@ class Board extends React.Component {
   }
 
   render () {
-
-    const status = this.state.xIsNext ? 'X' : 'O';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = ' And the Winner: ' + winner;
+    } else {
+      status = 'Next: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -51,6 +56,29 @@ class Board extends React.Component {
       </div>
     )
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++){
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+      return squares[a];
+    }
+  }
+
+  return null;
+
 }
 
 export default Board
